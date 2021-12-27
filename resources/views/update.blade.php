3984558b6be3e35e4b8a9d43e2d335dbdca58793
佐藤新奈 <satoushinna@satoushinnanoMacBook-Air.local>
2021-12-27 15:08:24 +0900

    e

--- resources/views/update.blade.php
+++ resources/views/update.blade.php
@@ -12,14 +12,14 @@
 
 @if(!empty($users))
   <tr class="o">
-    <th>ID</th><th colspan="1">name</th><th colspan="1">gender</th><th colspan="1">email</th><th colspan="1">birthday</th><th colspam="1">phone number</th><th colspam="1">area</th>
+    <th></th><th colspan="1">name</th><th colspan="1">gender</th><th colspan="1">email</th><th colspan="1">birthday</th><th colspam="1">phone number</th><th colspam="1">area</th>
   </tr>
   @foreach($users as $user)
   <form action="{{ route('update', ['id' => $user->id])}}" method="post">
   @csrf
   
   <tr>
-  <td><input type="hidden" name="id" id="{{$user->id}}"></td><td><input type="text" class="update" value="{{$user->name}}" name="name"></td><td>{{$user -> gender}}</td><td><input type="text" class="update" value="{{$user->email}}" name="email"></td><td>{{$user -> birthday}}</td><td><input type="text" class="update" value="{{$user->phone_number}}" name="phone_number"></td><td>{{$user -> workarea}}</td><td></td>
+  <td><input type="hidden" class="update" value="{{$user->id}}" name="id"></td><td><input type="text" class="update" value="{{$user->name}}" name="name"></td><td>{{$user -> gender}}</td><td><input type="text" class="update" value="{{$user->email}}" name="email"></td><td>{{$user -> birthday}}</td><td><input type="text" class="update" value="{{$user->phone_number}}" name="phone_number"></td><td>{{$user -> workarea}}</td><td></td>
   </tr>
   @endforeach
 </table>
